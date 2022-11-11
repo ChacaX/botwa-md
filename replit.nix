7a24be9ea65f601e8a996f665c612cf9aee6d83e
MITSUHA-XXX <77813696+ChacaX@users.noreply.github.com>
2022-11-12 00:39:09 +0700

    Create replit.nix

---
+++ replit.nix
@@ -0,0 +1,14 @@
+{ pkgs }: {
+    deps = [
+        pkgs.nodejs
+        pkgs.nodePackages.typescript
+        pkgs.ffmpeg
+        pkgs.imagemagick
+        pkgs.git
+    ];
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
